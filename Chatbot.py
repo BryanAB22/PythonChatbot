90c34a7d45a954ee6162695cb30745f7ede2f616
BryanAB22 <117081507+BryanAB22@users.noreply.github.com>
2023-04-24 13:27:30 -0500

    Add files via upload

---
+++ Chatbot.py
@@ -0,0 +1,30 @@
+import random
+
+greetings=["Hello!","What's up?!", "Howdy! ","Greetings!"]
+goodbyes=["Bye!", "Goodbye!", "See you later!", "See you soon!"]
+keywords=["music", "pet", "book", "game"]
+responses=["Music is so relaxing!","Dogs are man's best friend!", "I know a lot about books", "I like to play video games"]
+
+print(random.choice(greetings))
+
+user = input("Say something (or type bye to quit): ")
+user = user.lower()
+
+while user != "bye":
+    keyword_found = False
+    for index in range(len(keywords)):
+        if keywords[index] in user:
+            print("Bot: " + responses[index])
+            keyword_found = True
+            break
+    if not keyword_found:
+        new_keyword = input("I'm not sure how to respond. What keyword should I respond to?")
+        keywords.append(new_keyword)
+        new_response = input("How should I respond to " + new_keyword + "?")
+        responses.append(new_response)
+        break  # Exit the loop after adding the new keyword and response.
+
+    user = input("Say something (or type bye to quit): ")
+    user = user.lower()
+
+print(random.choice(goodbyes))
